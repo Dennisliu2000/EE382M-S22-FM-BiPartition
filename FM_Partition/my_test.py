4715cc0a1c69feda9bf3cfdf0ca3bd6933c8c7ce
Dennisliu2000 <47581177+Dennisliu2000@users.noreply.github.com>
2022-02-14 22:50:02 -0600

    work

---
+++ FM_Partition/my_test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 14 20:08:03 2022
+
+@author: denni
+"""
+
+import os
+from student_impl.eid_dl34437_EID import FM_Partition
+eid = "dl34437"
+benchmark_path = "benchmarks/example_1.txt"
+output_root = "output"
+output_root = os.path.join(output_root, eid)
+if not os.path.isdir(output_root):
+    os.mkdir(output_root)
+
+output_path = os.path.join(output_root, os.path.basename(benchmark_path))
+solver = FM_Partition()
+solver.read_graph(benchmark_path)
+solution = solver.solve()
+profiling = (0, 0) # ignore runtime and memory for now
+solver.dump_output_file(*solution, *profiling, output_path)
--- FM_Partition/student_impl/eid_dl34437_EID.py
+++ FM_Partition/student_impl/eid_dl34437_EID.py
@@ -21,6 +21,9 @@ class FM_Partition(FM_Partition_Base):
         super().__init__()
     
     def update_gbuckets(self):                                  # Calculates gains for all nodes - technically only neighbors are needed, but this is what I have for now
+
+        self.block0_g.clear()
+        self.block1_g.clear()
         
         for n in range(len(self.partition[0])):                 # Computing gains for each node in block0 - moves each node in block0 to block1, calculates cut size diff, then moves them back
             if self.partition[0][n] in self.locked_nodes:       # If the current node we are looking at is in the locked nodes list, just ignore it
@@ -30,13 +33,13 @@ class FM_Partition(FM_Partition_Base):
                 self.partition[0].pop(n)
                 temp_cut_size = self.compute_cut_size(self.partition)
                 temp_cut_size = self.cut_size - temp_cut_size
+                self.partition[0].insert(n, self.partition[1][-1])
+                self.partition[1].pop()
                 if temp_cut_size in self.block0_g.keys():
                     self.block0_g.get(temp_cut_size).append(self.partition[0][n])
                 else:
                     self.block0_g[temp_cut_size] = [self.partition[0][n]]
-                self.partition[0].insert(n, self.partition[1][-1])
-                self.partition[1].pop()
-            
+        
         for n in range(len(self.partition[1])):                 # Computing gains for each node in block1
             if self.partition[1][n] in self.locked_nodes:
                 continue
@@ -44,70 +47,96 @@ class FM_Partition(FM_Partition_Base):
                 self.partition[0].append(self.partition[1][n])
                 self.partition[1].pop(n)
                 temp_cut_size = self.compute_cut_size(self.partition)
+                temp_cut_size = self.cut_size - temp_cut_size
+                self.partition[1].insert(n, self.partition[0][-1])
+                self.partition[0].pop()
                 if temp_cut_size in self.block1_g.keys():
                     self.block1_g.get(temp_cut_size).append(self.partition[1][n])
                 else:
                     self.block1_g[temp_cut_size] = [self.partition[1][n]]
-                self.partition[1].insert(n, self.partition[0][-1])
-                self.partition[0].pop()        
+    
+    def update_gnodes(self):
         
-        
-        raise NotImplementedError
+        listk = []
+        listk1 = []
+
+        for key, lists in self.block0_g.items():
+            for l_nodes in self.locked_nodes:
+                if l_nodes in lists:
+                    lists.remove(l_nodes)
+                    if not lists:
+                        listk.append(key)
+                        
+        for key in listk:
+            self.block0_g.pop(key)
+                    
+        for key, lists in self.block1_g.items():
+            for l_nodes in self.locked_nodes:
+                if l_nodes in lists:
+                    lists.remove(l_nodes)
+                    if not lists:
+                        listk1.append(key)
+                    
+        for key in listk1:
+            self.block1_g.pop(key)
         
     def move_rtl(self):
         
-        x = self.block1_g.keys().sort()                 # Get all gains in block1 and sort
+        #x = sorted(self.block1_g.keys())                 # Get all gains in block1 and sort
         
-        biggest_gains = self.block1_g.get(x[-1])        # Get the highest gain's list of nodes
+        biggest_gains = self.block1_g.get(max(self.block1_g))        # Get the highest gain's list of nodes
         biggest_gains.sort()
         self.locked_nodes.append(biggest_gains[0])
         self.partition[0].append(biggest_gains[0])      # Add in the first node to the "left" (block0)
         self.partition[1].remove(biggest_gains[0])      # Remove the node from the "right" (block 1)
         
-        raise NotImplementedError
         
     def move_ltr(self):                                 # Same as move_rtl, but the blocks have been switched
         
-        x = self.block0_g.keys().sort()
+        #x = sorted(self.block0_g.keys())
         
-        biggest_gains = self.block0_g.get(x[-1])
+        biggest_gains = self.block0_g.get(max(self.block0_g))
         biggest_gains.sort()
         self.locked_nodes.append(biggest_gains[0])
         self.partition[1].append(biggest_gains[0])
         self.partition[0].remove(biggest_gains[0])
         
-        raise NotImplementedError
         
     def move(self):
         
-        x = self.block1_g.keys().sort()     
-        y = self.block0_g.keys().sort()
+        #x = sorted(self.block1_g.keys())  
+        #y = self.block0_g.keys().sort()
         
-        if x[-1] > y[-1]:       # If the largest gain on block1 is greater than the largest gain in block0, perform a right-to-left move
+        if max(self.block1_g) > max(self.block0_g):       # If the largest gain on block1 is greater than the largest gain in block0, perform a right-to-left move
             self.move_rtl()
-        elif y[-1] > x[-1]:     # Vice-versa
+        elif max(self.block0_g) > max(self.block1_g):     # Vice-versa
             self.move_ltr()
         else:                   # Both blocks have an equal maximum gain; find the first node via node number tie-breaker and then perform a rtl or ltr move
-            a = self.block0_g.get(x[-1]).sort()
-            b = self.block1_g.get(y[-1]).sort()
+            print(self.block0_g)
+            print(self.block1_g)
+            print(max(self.block0_g))
+            print(max(self.block1_g))
+            a = self.block0_g.get(max(self.block0_g))
+            b = self.block1_g.get(max(self.block1_g))
+            print(a)
+            print(b)
             if a[0] < b[0]:
                 self.move_rtl()
             else:
                 self.move_ltr()
-        
-        raise NotImplementedError        
+             
         
     def initialize(self):
         """Initialize necessary data structures before starting solving the problem
         """
         self.cut_size = 0
         self.cut_size_list = []
-        self.best_sol
+        self.best_sol = ()
         self.best_cut_size = 0
         self.locked_nodes = []
         self.block0 = []
         self.block1 = []
-        self.r_ep
+        self.r_ep = 0
         self.block0_g = {}
         self.block1_g = {}
         
@@ -144,9 +173,14 @@ class FM_Partition(FM_Partition_Base):
         # Initialize gain buckets
         self.update_gbuckets()
         
+        print(self.partition)
+        print(self.block0_g)
+        print(self.block1_g)
+        print(self.cut_size)
+            
         # TODO initialize any auxiliary data structure you need
         # e.g., node2net_map, cell gains, locked cells, etc.
-        raise NotImplementedError
+
 
     def partition_one_pass(self) -> Tuple[List[int], Tuple[List[str], List[str]], int]:
         """FM graph partition algorithm for one pass
@@ -163,15 +197,22 @@ class FM_Partition(FM_Partition_Base):
         # But do not override methods in the parent class
         # Please strictly follow the return type requirement.
         
-        # (m.min(len(self.partition[0]), len(self.partition[1])) / self.n_nodes) < r_ep
+        # (m.min(len(self.partition[0]), len(self.partition[1])) / self.n_nodes) < r_ep      
         while len(self.locked_nodes) < self.n_nodes:                        # As long as the locked-nodes list does not have all the nodes, we can iterate and swap
+            print(self.partition)
+            print(self.block0_g)
+            print(self.block1_g)
+            print(self.cut_size)
             if ((len(self.partition[1]) - 1) / self.n_nodes) < self.r_ep:   # If a right-to-left (block1 to block0) move would make the right partition too small, then execute a left-to-right move
                 self.move_ltr()
             elif ((len(self.partition[0]) - 1) / self.n_nodes) < self.r_ep: # If a left-to-right move would make the left partition too small, then execute a right-to-left move
                 self.move_rtl()
             else:                                                           # If both moves are acceptable, then find the best move out of both sides
+                print("I AM MOVING - I AM MOVING - I AM MOVING - I AM MOVING - I AM MOVING - I AM MOVING - I AM MOVING - I AM MOVING - ")
                 self.move()
                 
+            #self.update_gnodes()
+            
             self.update_gbuckets()                                          # After a move, the gains are re-calculated
             self.cut_size = self.compute_cut_size(self.partition)           # Add the new cutsize to the history; check to see if we have a new best
             self.cut_size_list.append(self.cut_size)
@@ -180,7 +221,11 @@ class FM_Partition(FM_Partition_Base):
                 self.best_cut_size = self.cut_size
                 self.best_sol = ([self.node2node_name_map[n] for n in self.partition[0]], [self.node2node_name_map[n] for n in self.partition[1]])
                 
+            #print(self.partition)
+            #print(self.block0_g)
+            #print(self.block1_g)
+            #print(self.cut_size)
+                
         else: 
             return self.cut_size_list, self.best_sol, self.best_cut_size    # No moves available, first pass is over. Return gathered values.
         
-        raise NotImplementedError
